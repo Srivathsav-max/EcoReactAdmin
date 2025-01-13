2ff3abaa1a1e3516afa22946501c3e25671ff666
Jaya Raj Srivathsav Adari <190030018@kluniversity.in>
2025-01-13 13:40:34 -0500

    footer change

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="bg-white dark:bg-slate-950 border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black dark:text-white">
-          &copy; 2025 Sonic Solutions, Inc. All rights reserved.
+          &copy; 2025 All rights reserved.
         </p>
       </div>
     </footer>
