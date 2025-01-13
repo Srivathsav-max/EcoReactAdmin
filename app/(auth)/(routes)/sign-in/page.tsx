fe5f882e248c49c3653115ee8aaeba98682c39f3
Jaya Raj Srivathsav Adari <190030018@kluniversity.in>
2025-01-13 01:30:18 -0500

    fixed ESLint Error

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -117,7 +117,7 @@ export default function SignInPage() {
         </CardContent>
         <CardFooter>
           <p className="text-center text-sm text-gray-600 w-full">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link 
               href={`/sign-up${redirectUrl !== "/" ? `?redirect=${redirectUrl}` : ""}`}
               className="font-medium text-blue-600 hover:text-blue-500"
